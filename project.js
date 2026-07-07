7dba0ea55dc5aeda2ef4577cb58e39b90fab07dd
Antonin LECUYER <antonin.lecuyer.pro@gmail.com>
2026-07-07 15:35:03 +0200

    Adding addTodo, getTodos and removeTodo

--- project.js
+++ project.js
@@ -1,9 +1,31 @@
 
 function newProject (nameOfProject,todos = []){
+    let todos = [];
+
     return{
         nameOfProject,
-        todos 
+        addTodo: (todo) =>{
+            if(todo && todo.id && todo.title){
+                todos.push(todo);
+            }else{
+                console.log("Not a Todo in entry")
+            }
+            
+        },
+        getTodos: () =>{
+            const copyTodos = [...todos];
+            return copyTodos;
+        },
+        removeTodo: (id) =>{
+            todos = todos.filter((todo) => todo.id != id);
+        }
+
     }
+
+}
+
+function addTodoToProject(project,todo){
+    project.addTodo(todo);
 
 }
 
