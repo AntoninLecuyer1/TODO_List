271d60b2f4212ae6f6e616b3f2139b2065115e2b
Antonin LECUYER <antonin.lecuyer.pro@gmail.com>
2026-07-07 16:52:36 +0200

    Tests completed and validated

--- project.js
+++ project.js
@@ -1,4 +1,4 @@
-export function newProject (nameOfProject,todos = []){
+export function newProject (nameOfProject){
     let todos = [];
 
     return{
--- test.js
+++ test.js
@@ -1,2 +1,16 @@
 import {newTodo} from "./todo.js";
-import { newProject } from "./project";
+import {newProject} from "./project.js";
+
+
+const project = newProject("Brassage IPA");
+const todo1 = newTodo("Acheter du houblon", "desc", "2025-01-01", "haute");
+
+
+project.addTodo(todo1);
+
+
+console.log(project.getTodos().length === 1 ? "addTodo OK" : "addTodo FAIL");
+
+project.removeTodo(todo1.id);
+
+console.log(project.getTodos().length === 0 ? "removeTodo OK" : "removeTodo FAIL");
