04d47608ab05c8e078206cef4cc0d0ff087b26d2
Antonin LECUYER <antonin.lecuyer.pro@gmail.com>
2026-07-08 14:57:50 +0200

    Ading Date format

--- todo.js
+++ todo.js
@@ -1,11 +1,12 @@
 export function newTodo(title,description,dueDate,priority,status = false){
     const id = Math.floor(Math.random()*1000);
-    
+    const dueDateConv = new Date(dueDate);
+
     return{
         id,
         title,
         description,
-        dueDate,
+        dueDateConv,
         priority,
         status
     }
